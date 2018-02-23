af5ecadc820a9a2c0d20f00d30d4917bc79e08bd
Sam Watkinson <sam.ws.watkinson@gmail.com>
2018-02-22 16:36:52 -0800

    fix a bug where existing search results would lose previews when loaded fully

--- client/src/State/Cases/reducer.js
+++ client/src/State/Cases/reducer.js
@@ -12,6 +12,7 @@ export default (state = {}, action) => {
       return {
         ...state,
         [action.payload.id]: {
+          ...state[action.payload.id],
           ...action.payload,
           fullContentLoaded: true
         }
